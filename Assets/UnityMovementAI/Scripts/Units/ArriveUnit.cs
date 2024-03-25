0985979c4a1334560367086c61be53bdbb70b91b
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-03-25 02:53:40 -0400

    addedAI

---
+++ Assets/UnityMovementAI/Scripts/Units/ArriveUnit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityMovementAI
+{
+    public class ArriveUnit : MonoBehaviour
+    {
+
+        public Vector3 targetPosition;
+        public bool wanderer = false;
+        public float wanderRadius = 10f;
+
+        SteeringBasics steeringBasics;
+
+        void Start()
+        {
+            steeringBasics = GetComponent<SteeringBasics>();
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 accel = steeringBasics.Arrive(targetPosition);
+
+            steeringBasics.Steer(accel);
+            steeringBasics.LookWhereYoureGoing();
+
+            if (wanderer & Vector3.Distance(transform.position, targetPosition) < .5f)
+            { targetPosition = Random.insideUnitSphere * Random.Range(0f,wanderRadius); }
+        }
+    }
+}
---
+++ Assets/UnityMovementAI/Scripts/Units/WanderAvoidUnit.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace UnityMovementAI
+{
+    public class WanderAvoidUnit : MonoBehaviour
+    {
+        SteeringBasics steeringBasics;
+        Wander2 wander;
+        CollisionAvoidance colAvoid;
+
+        NearSensor colAvoidSensor;
+
+        public bool yWander;
+        public float yWobble;
+
+        void Start()
+        {
+            steeringBasics = GetComponent<SteeringBasics>();
+            wander = GetComponent<Wander2>();
+            colAvoid = GetComponent<CollisionAvoidance>();
+
+            colAvoidSensor = transform.Find("ColAvoidSensor").GetComponent<NearSensor>();
+        }
+
+        void FixedUpdate()
+        {
+
+            Vector3 accel = colAvoid.GetSteering(colAvoidSensor.targets);
+            if (yWander)
+            {
+                accel.y += Random.Range(-yWobble, yWobble);
+            }
+
+            if (accel.magnitude < 0.005f)
+            {
+                accel = wander.GetSteering();
+            }
+
+            steeringBasics.Steer(accel);
+            steeringBasics.LookWhereYoureGoing();
+        }
+    }
+}
