46cfe9399990a50805154ee908af1c90b6283302
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-04-22 13:42:08 -0400

    inProgressClassDemo

--- Assets/SRP and Post/newCustomRenderFeature.cs
+++ Assets/SRP and Post/newCustomRenderFeature.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class CustomPostRenderFeature : ScriptableRendererFeature
+public class newCustomRenderFeature : ScriptableRendererFeature
 {
-
     public Shader myBloomShader;
     public float scatter;
     public int clamp;
     public float threshold;
     Material myBloomMat;
-    CustomPostPass myPass;
-
+    newCustomPass myPass;
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         if (renderingData.cameraData.cameraType == CameraType.Game)
@@ -24,27 +22,23 @@ public class CustomPostRenderFeature : ScriptableRendererFeature
 
     public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
     {
-        //render on game camera, not in editor
-        if(renderingData.cameraData.cameraType == CameraType.Game)
-        {
-            myPass.ConfigureInput(ScriptableRenderPassInput.Depth);
-            myPass.ConfigureInput(ScriptableRenderPassInput.Color);
-            myPass.SetTarget(renderer.cameraColorTargetHandle, renderer.cameraDepthTargetHandle);
-        }
+        myPass.ConfigureInput(ScriptableRenderPassInput.Depth);
+        myPass.ConfigureInput(ScriptableRenderPassInput.Color);
+        //myPass.SetTarget(renderer.cameraColorTargetHandle, renderer.cameraDepthTargetHandle);
     }
 
     public override void Create()
     {
         myBloomMat = CoreUtils.CreateEngineMaterial(myBloomShader);
-        myPass = new CustomPostPass(myBloomMat);
+        myPass = new newCustomPass(myBloomMat);
         myPass.clamp = clamp;
         myPass.threshold = threshold;
-        myPass.scatter = scatter;  
+        myPass.scatter = scatter;
+        
     }
 
     protected override void Dispose(bool disposing)
     {
-        CoreUtils.Destroy(myBloomMat);
+        
     }
-
 }
