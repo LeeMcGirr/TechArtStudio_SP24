5cf4d69252c7dccd015da1807f94fdf5e43ef0da
LeeMcGirr <89796414+LeeMcGirr@users.noreply.github.com>
2025-04-25 01:37:18 -0400

    added fullscreen shadergraph

---
+++ Assets/ShaderDemos/KaleidoscopeEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName = "Image Effects Ultra/Kaleidoscope", order = 1)]
+public class KaleidoscopeEffect : ScriptableObject
+{
+    public int segments = 4;
+    Material baseMaterial;
+
+    // Find the Kaleidoscope shader source.
+    public void OnCreate()
+    {
+        baseMaterial = new Material(Resources.Load<Shader>("Shaders/Kaleidoscope"));
+        baseMaterial.SetFloat("_SegmentCount", segments);
+    }
+
+    public void Render(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, baseMaterial);
+    }
+}
